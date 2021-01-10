bd4678d966846a2229d3189d3625cd6a8adfc334
viorellu <44996636+viorellu@users.noreply.github.com>
2021-01-10 15:32:03 +0200

    Update userinput.sh

--- userinput.sh
+++ userinput.sh
@@ -9,19 +9,19 @@ read -p "Introdu pretul maxim dorit (nr. intreg, default este 3000): " price
 price=${price:-3000}
 read -p "La cate secunde sa se faca refresh-ul (default este 10): " sec
 sec=${sec:-10}
-echo "Se pare ca azi cautam un pret mai mic de $price lei pentru" $(echo $url | cut -f4 -d "/" | tr "_" " ") in sectiunea $(echo $url | cut -f5 -d "/")". Refresh-ul rezultatelor se va face la fiecare $sec secunde."
+echo "Se pare ca azi cautam un pret mai mic de "$price" lei pentru" $(echo $url | cut -f 5 -d "/" | awk -F "?" '{print $1}' | sed "s/%20/ /" | tr "+" " " | tr [:lower:] [:upper:] ) "in sectiunea" $(echo $url | cut -f4 -d "/" | tr "_" " " | tr [:lower:] [:upper:])". Refresh-ul rezultatelor se va face la fiecare "$sec" secunde."
 while true
 do
-	curl -s $url | tr "," "\n" | grep -e "product_name&quot" -e "quot;price&quot" | awk 'NR%2{printf "%s ",$0;next;}1' | awk -v kw=$kw 'tolower($0) ~ tolower(kw)' | awk -F ";" '{print $4 "     " $NF}' | awk '{print $NF,$0}' | sort -u | sort -n | cut -f2- -d' ' | sed -e "s/&quot//" -e "s/u00ae//" -e "s/u2122//" -e "s/&#039//" | tr \\ " " 2>/dev/null | while read -r line
+        curl -s $url | tr "," "\n" | grep -e "product_name&quot" -e "quot;price&quot" | awk 'NR%2{printf "%s ",$0;next;}1' | awk -v kw=$kw 'tolower($0) ~ tolower(kw)' | awk -F ";" '{print $4 "     " $NF}' | awk '{print $NF,$0}' | sort -u | sort -n | cut -f2- -d' ' | sed -e "s/&quot//" -e "s/u00ae//" -e "s/u2122//" -e "s/&#039//" | tr \\ " " 2>/dev/null | while read -r line
     do
     p=$(echo $line | cut -f 2 -d ":" | cut -f1 -d ".")
     if [ $p -gt $price ]
-		then
-			echo `date +"%d.%h.%y %H:%M:%S"` "Bad price found for" $line
-	else
-			echo `date +"%d.%h.%y %H:%M:%S"` "Good price found for" $line
-	fi
-	done | sort -k3
+                then
+                        echo `date +"%d.%h.%y %H:%M:%S"` "Bad price found for" $line
+        else
+                        echo `date +"%d.%h.%y %H:%M:%S"` "Good price found for" $line
+        fi
+        done | sort -k3
 echo =======================================
 sleep $sec
 done
